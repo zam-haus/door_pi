dce8f67f0696045f9568fa491b4cfd79af493386
Christian Carlowitz <cc@cmesh.de>
2021-09-02 23:39:14 +0200

    added simple door_manager mqtt interface

--- door_manager/door_manager.py
+++ door_manager/door_manager.py
@@ -15,8 +15,54 @@
 # You should have received a copy of the GNU General Public License
 # along with door_manager.  If not, see <http://www.gnu.org/licenses/>.
 
+''' door_manager
+
+Usage:
+  door_manager.py
+  door_manager.py (-s | --simulate)
+
+Options:
+  -s --simulate     Use console I/O for HAL instead of RPi GPIOs
+'''
+
+import sys
 from json import loads
-import paho.mqtt.client as mqtt
+from time import time, sleep
+from signal import pause
+from docopt import docopt
+from decorated_paho_mqtt import GenericMqttEndpoint
+from door_hal import DoorHal, DoorHalSim
 
+config = loads(open('config.json').read())
 
+class DoorManager(GenericMqttEndpoint):
+    def __init__(self, hal, client_kwargs: dict, password_auth: dict, server_kwargs: dict, tls: bool):
+        super().__init__(client_kwargs, password_auth, server_kwargs, tls)
+        self.hal = hal
+    
+    @GenericMqttEndpoint.subscribe_decorator('door/%s/open' % config['door-id'], qos=2)
+    def open(self, *, client, userdata, message):
+        payload = loads(message.payload)
+        assert 'not_after' in payload
+        if time() < float(payload['not_after']):
+            self.hal.setOutput('key', 1)
+            sleep(0.5)
+            self.hal.setOutput('key', 0)
 
+if __name__ == '__main__':
+    args = docopt(__doc__)
+    
+    if args['--simulate']:
+        hal = DoorHalSim()
+    else:
+        hal = DoorHal()
+        
+    dm = DoorManager(
+        hal,
+        dict(transport='tcp'),
+        dict(username=config['mqtt-user'], password=config['mqtt-pass']),
+        dict(host=config['mqtt-host'], port=config['mqtt-port'], keepalive=10),
+        False
+    )
+    dm.connect()
+    pause()
